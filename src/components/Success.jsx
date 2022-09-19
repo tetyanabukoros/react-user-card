b2973246ab131722b8b65a0232e8ad3249ad5677
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-09-19 18:57:00 +0300

    conditional success text added

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -6,8 +6,15 @@ export const Success = ({ count }) => {
   return (
     <div class="success-block">
       <img src={success} alt="Success" />
-      <h3>Успешно!</h3>
-      <p>All {count} users have been invited.</p>
+      <h3>Success!</h3>
+      {
+        count === 1 ? (
+          <p> Invitation sent to {count} user.</p>
+        ) : (
+          <p> Invitation sent to {count} users.</p>
+        )
+      }
+     
         <button onClick={() => window.location.reload()} className="send-invite-btn">Back</button>
     </div>
   );
